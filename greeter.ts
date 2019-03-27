8044079af3a85f3300afb67098fd24433074b6a6
lvsuzhen <lvsz@boqii.com>
2019-03-27 21:38:32 +0800

    ts

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,14 @@
-function greeter(person) {
-  return "Hello, " + person;
+class Student {
+  fullName: string;
+  constructor(public aName,public bName) {
+    this.fullName = 'aName:'+this.aName+''+' bName:'+this.bName
+  }
 }
-
-let user = "Jane User";
-
-document.body.innerHTML = greeter(user);
+interface User {
+  aName: string;
+  bName: string;
+}
+function greeter (user: User) {
+  return 'hello'+user.aName+'hello'+user.bName 
+}
+console.log(greeter(new Student('aaaaaa','bbbbbbb')))
